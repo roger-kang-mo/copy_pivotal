41b53893e859af69ff6d49c35de32507c76e8581
Roger Mo <roger.kang.mo@gmail.com>
2019-06-14 12:31:25 -0700

    Run on page load if the URL matches or if clicking on new PR from main page

--- copy_link.js
+++ copy_link.js
@@ -1,13 +1,45 @@
-var currentURL = window.location.href;
-var storyID = currentURL.substring(currentURL.indexOf("/compare") + 9);
-storyID = storyID.substring(storyID.indexOf("/") + 1);
-storyID = storyID.substring(0, storyID.indexOf("-"));
+function copyPivotalStoryIDToPR() {
+  var waitCount = 0;
 
-var prBody = document.querySelector("#pull_request_body");
+  var prBodyPresentInterval = setInterval(function () {
+    var prBody = document.querySelector("#pull_request_body");
+    waitCount++;
 
-if (prBody.value.indexOf("[*Pivotal Story*](https://www.pivotaltracker.com/story/show/)") != -1) {
-  var newPivotalLink = "[*Pivotal Story*](https://www.pivotaltracker.com/story/show/" + storyID + ")";
-  var newPrBody = prBody.value.replace("[*Pivotal Story*](https://www.pivotaltracker.com/story/show/)", newPivotalLink);
+    if (prBody) {
+      clearInterval(prBodyPresentInterval);
 
-  prBody.value = newPrBody;
+      var currentURL = window.location.href;
+      console.log(currentURL);
+      var storyID = currentURL.substring(currentURL.indexOf("/compare") + 9);
+      storyID = storyID.substring(storyID.indexOf("/") + 1);
+      storyID = storyID.substring(0, storyID.indexOf("-"));
+      console.log(storyID);
+
+      var prBody = document.querySelector("#pull_request_body");
+
+      console.log(prBody);
+
+      if (prBody.value.indexOf("[*Pivotal Story*](https://www.pivotaltracker.com/story/show/)") != -1) {
+        var newPivotalLink = "[*Pivotal Story*](https://www.pivotaltracker.com/story/show/" + storyID + ")";
+        var newPrBody = prBody.value.replace("[*Pivotal Story*](https://www.pivotaltracker.com/story/show/)", newPivotalLink);
+
+        prBody.value = newPrBody;
+      }
+    } else if (waitCount > 20) {
+      clearInterval(prBodyPresentInterval);
+    }
+  }, 500);
+}
+
+if (window.location.href.indexOf("/compare/") !== -1 && window.location.href.indexOf("...") == -1) {
+  window.onload = copyPivotalStoryIDToPR();
+} else if (window.location.href.indexOf("/tree/" !== -1) {
+  window.onload = function () {
+    var prButton = document.querySelector(".new-pull-request-btn");
+    if (prButton) {
+      prButton.addEventListener("click", function () {
+        copyPivotalStoryIDToPR();
+      });
+    }
+  }
 }
